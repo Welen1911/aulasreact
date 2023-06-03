860d54435d4d20bcb879c636f4c8128a5951b34e
Welen1911 <welenalmeida2210@gmail.com>
2023-06-03 15:55:11 -0300

    feat: estruturação de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-
+import React, {useState} from "react"
 export const App = () => {
   return (
     <div className="App">
       teste
+      <br />
+      <input type="text" id="num1" placeholder="Digite o primeiro número"/>
+      <input type="text" id="num2" placeholder="Digite o segundo número"/>
+      <button>Enviar</button>
     </div>
   );
+}
+function somar(valor1: number, valor2: number) {
+  let resultado: number = valor1 + valor2
+  return (
+    <div>
+      {resultado}
+    </div>
+  )
 }
